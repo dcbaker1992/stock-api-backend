4433eebeb878905c937d05da0829e4aa589148fb
Dillon C Baker <73920614+dcbaker1992@users.noreply.github.com>
2021-12-21 14:43:28 -0600

    initial commit of build backend

---
+++ app/funds/routes.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../../lib/db')
+
+router.get('/funds', (req, res) => {
+  const funds = db.funds.findAll().slice(0).sort((a, b) => a.name > b.name ? 1 : 0)
+  console.log("htting the funds route", funds[0]);
+  res.json(funds)
+})
+
+router.get('/funds/:id', (req, res) => {
+  const fund = db.funds.find(req.params.id)
+  res.json(fund)
+})
+
+router.post('/funds', (req, res) => {
+  const newFund = db.funds.insert({
+    "company": req.body.company,
+    "open": req.body.open,
+    "volume": req.body.volume,
+    "todaysRange": req.body.todaysRange,
+    "yearlyRange": req.body.yearlyRange,
+    "martketCap": req.body.martketCap,
+    "sector": req.body.sector,
+    "industry": req.body.industry,
+    "headquarters": req.body.headquarters
+  })
+  res.json(newFund)
+})
+
+router.put('/funds/:id', (req, res) => {
+  const updatedFund = db.funds.findByIdAndUpdate(req.params.id, {
+    "company": req.body.company,
+    "open": req.body.open,
+    "volume": req.body.volume,
+    "todaysRange": req.body.todaysRange,
+    "yearlyRange": req.body.yearlyRange,
+    "martketCap": req.body.martketCap,
+    "sector": req.body.sector,
+    "industry": req.body.industry,
+    "headquarters": req.body.headquarters
+  })
+  res.json(updatedBook)
+})
+
+// router.patch('/books/cart/add/:id', function (req, res) {
+//   const book = db.books.find(req.params.id)
+//   book.inCart = true
+//   res.json(book)
+// })
+
+// router.patch('/books/cart/remove/:id', function (req, res) {
+//   const book = db.books.find(req.params.id)
+//   book.inCart = false
+//   res.json(book)
+//})
+
+router.patch('/funds/:id', (req, res) => {
+   const fund = db.funds.findByIdAndUpdate(req.params.id, req.body)
+   res.json(db.funds.findAll())
+})
+
+router.delete('/funds/:id', (req, res) => {
+  let selectedFund = db.funds.find(req.params.id)
+  db.funds.delete(req.params.id)
+  res.json(selectedFund)
+
+})
+
+
+
+module.exports = router
